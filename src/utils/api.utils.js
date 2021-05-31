0d04fb55d8001d05440fc4d52a0619e5366bc7e9
Giuseppe Albrizio <g.albrizio@gmail.com>
2021-05-31 16:50:39 +0200

    axios multiple endpoints

--- src/utils/api.utils.js
+++ src/utils/api.utils.js
@@ -6,4 +6,9 @@ export default {
     baseURL: 'endpoint1-url',
     headers: { 'Content-Type': 'application/json' },
   }),
+  endpoint2: axios.create({
+    withCredentials: true,
+    baseURL: 'endpoint1-url',
+    headers: { 'Content-Type': 'application/json' },
+  }),
 };
